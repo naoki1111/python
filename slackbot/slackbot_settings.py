526c369db3b60810e4d3ba368291b8df191ab670
naoki1111 <jackpod62534@gmail.com>
2019-03-11 17:19:19 +0900

    slackbot default

---
+++ slackbot/slackbot_settings.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-571702884720-572788282725-U7WVxjJlipDrnhnxhi87IMfj"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "what?"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+#PLUGINS = ['plugins']
